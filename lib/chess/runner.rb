4de1a8dfed3587c6ec7859427c637638a734ac1d
dinoflower <kaedilove@gmail.com>
2023-05-16 17:08:07 -0500

    create runner class and add script to bin file

---
+++ lib/chess/runner.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative 'game'
+
+# The file in bin creates an instance of this class to run the game.
+class Runner
+  def initialize
+    # @arguments = arguments
+    # @stdin = stdin
+    # @stdout = stdout
+    @game = Game.new
+  end
+
+  def run
+    # later, implement config for game load?
+    # Config.in_stream = @stdin
+    # Config.out_stream = @stdout
+    # parse_options
+    @game.play_game
+  end
+
+  # e.g. to start a new game or load a save?
+  # def parse_options
+    # options = OptionParser.new 
+    # options.banner = "Usage: chess [options]"
+    # options.on('-h', '--help',          "Show this message")          { puts(options); exit }
+    # options.parse!(@arguments)
+  # end
+end
